4b77a33b7b30839eb39928214fe5c41161340f1c
amateurreyganz <ganzonreymark879@gmail.com>
2026-07-15 12:18:26 +0800

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-const btn = document.getElementById("btn");
+ let total = 0;
+let items = 0;
 
-btn.addEventListener("click", () => {
-    alert("Welcome to Ganzon's Inventory POS!");
-});
+function addProduct(name, price){
+
+    const cart = document.getElementById("cart");
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `${name} - ₱${price}`;
+
+    cart.appendChild(li);
+
+    total += price;
+    items++;
+
+    document.getElementById("total").innerHTML = total;
+    document.getElementById("sales").innerHTML = total;
+    document.getElementById("items").innerHTML = items;
+}
